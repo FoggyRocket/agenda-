b330de29c07e71ada21e74dc91a0bc8d475fe572
Foggyrocket <dyl.blink.1.8.2@gmail.com>
2018-04-04 13:46:31 -0500

    cambi algo mas

--- src/components/project/DetailProjectPage.js
+++ src/components/project/DetailProjectPage.js
@@ -18,19 +18,40 @@ class DetailProjectPage extends Component {
 
     render() {
         const {project,fetched,tasks} = this.props;
+        console.log(tasks)
         const mytasks = tasks.map( task => ({
             id: task.id,
             text: task.name,
             start_date: moment(task.starts, moment.ISO_8601).format('DD-MM-YYYY'),
-            duration: moment(task.expiry, moment.ISO_8601).from(moment(task.starts, moment.ISO_8601)),
+            // duration: moment(task.expiry, moment.ISO_8601).from(moment(task.starts, moment.ISO_8601)),
+            duration:3,
             progress: 1
         }));
+        let newList = []
+        tasks.forEach(function(tasks) {
+            var singleObj = {}
+            singleObj['text'] = tasks.name;
+            singleObj['id'] = tasks.id;
+            singleObj['start_date']=moment(tasks.starts, moment.ISO_8601).format('DD-MM-YYYY');
+            singleObj['duration']=3;
+            singleObj['progress']=1;
+            newList.push(singleObj);
+        });
+        console.log(newList)
         const data = {
-            data: mytasks,
-            links: [
-                {}
-            ]
+            data: newList,
+            links: [     ]
+
+          // data:[
+          //     {id:9, text:"Saber como construir", start_date:"12-04-2018", duration:3},
+          //     // {id:2, text:"Task #1",       start_date:"12-04-2013", duration:3, parent:1},
+          //     // {id:3, text:"Alpha release",   parent:1,
+          //     //     start_date:"14-04-2013"},
+          //     // {id:4, text:"Task #2",       start_date:"17-04-2013", duration:3, parent:1}
+          //   ],
+          // links:[]
         };
+        console.log(data)
         return (
             <Fragment>
                 {
--- src/components/project/Gantt.js
+++ src/components/project/Gantt.js
@@ -3,28 +3,20 @@ import React, { Component } from 'react';
 import 'dhtmlx-gantt';
 import 'dhtmlx-gantt/codebase/locale/locale_es';
 import 'dhtmlx-gantt/codebase/dhtmlxgantt.css';
+import './ProjectStyles.css';
 
 export default class Gantt extends Component {
     componentDidMount() {
         gantt.init(this.ganttContainer);
         gantt.parse(this.props.tasks);
-    }
 
-    render() {
+    }
+    render(){
         return (
             <div
                 ref={(input) => { this.ganttContainer = input }}
                 style={{width: '100%', height: '100%'}}
             >
-            <div id="my-form">
-             <label for="description">Task text
-              <input type="text" name="description" value="" />
-             </label>
-             <input type="button" name="save" value="Save" />
-             <input type="button" name="close" value="Close" />
-             <input type="button" name="delete" value="Delete"/>
-            </div>
-
           </div>
         );
     }
